ff723db43c26ddf60022875a9162cc897120c59f
shio <sasimi35@gmail.com>
2016-04-05 01:32:16 +0900

    modified comment

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ git submodule update
 
 cd ../
 
-echo "Finished!\nYou have to install silversearcher-ag, powerline, and get powerline fonts.\nDon't forget to compile vimproc after update NeoBundle plugins.\nCaution! tmux settings are available since version 2.0"
+echo "Finished!\nYou have to install silversearcher-ag, virtualenv, powerline, and get powerline fonts.\nDon't forget to compile vimproc after update NeoBundle plugins.\nCaution! tmux settings are available since version 2.0"
