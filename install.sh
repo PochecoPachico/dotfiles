3b975e4db8d415c43a9c3dd8ae7f926ecddca401
shio <sasimi35@gmail.com>
2016-04-05 01:14:06 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,11 @@ cd .dotfiles
 
 git submodule init
 git submodule update
+
+cd .vim/bundle/vimproc
+
+make
+
+cd ../../../../
+
+echo "Finished!\nYou have to install silversearcher-ag, powerline, and get powerline fonts.\nCaution! these settings are available tmux 2.0 or higher"
