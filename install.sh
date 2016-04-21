b55e5ff664218049fc69dd8f38db85bf886198ae
shio <sasimi35@gmail.com>
2016-04-21 12:54:22 +0900

    modified shell script

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-cd ../
-
-mv dotfiles .dotfiles
-ln -s .dotfiles/.zshrc .zshrc
-ln -s .dotfiles/.vimrc .vimrc
-ln -s .dotfiles/.tmux.conf .tmux.conf
-ln -s .dotfiles/.vim .vim
-
-cd .dotfiles
-
 git submodule init
 git submodule update
 
 cd ../
 
-echo "Finished!\nYou have to install silversearcher-ag, virtualenv, powerline, and get powerline fonts.\nDon't forget to compile vimproc after update NeoBundle plugins.\nCaution! tmux settings are available since version 2.0"
+ln -s dotfiles/.zshrc .zshrc
+ln -s dotfiles/.vimrc .vimrc
+ln -s dotfiles/.tmux.conf .tmux.conf
+ln -s dotfiles/.vim .vim
+
+echo "Finished!\\nYou have to install silversearcher-ag, virtualenv, powerline, and get powerline fonts.\\nDon't forget to compile vimproc after update NeoBundle plugins.\\nCaution! tmux settings are available since version 2.0"
