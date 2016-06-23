28670df50f2a27d27303ef714ae9291b46a17eab
shio <sasimi35@gmail.com>
2016-06-24 00:12:32 +0900

    change message

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-
 git submodule init
 git submodule update
 
-cd ../
+ln -s .zshrc ../.zshrc
+ln -s .tmux.conf ../.tmux.conf
+ln -s .vimrc ../.vimrc
+ln -s .vimrc_light ../.vimrc_light
+ln -s .vim ../.vim
 
-ln -s .dotfiles/.zshrc .zshrc
-ln -s .dotfiles/.vimrc .vimrc
-ln -s .dotfiles/.vimrc_light .vimrc_light
-ln -s .dotfiles/.tmux.conf .tmux.conf
-ln -s .dotfiles/.vim .vim
-
-echo "Finished!\\nYou have to install silversearcher-ag, pyenv, powerline, and get powerline fonts.\\nPlease setup battery to display battery status.\\nDon't forget to compile vimproc after update NeoBundle plugins.\\nCaution! tmux settings are available since version 2.0"
+echo "Finished!\\nYou have to install silversearcher-ag and pyenv.\\nDon't forget to compile vimproc after update NeoBundle plugins.\\nCaution! tmux settings are available since version 2.0"
