1ccdab0df79bb9d87b5fb1556f99290e0c33d5e5
shio <sasimi35@gmail.com>
2016-06-24 00:21:30 +0900

    modified cd path

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 git submodule init
 git submodule update
 
-ln -s .zshrc ../.zshrc
-ln -s .tmux.conf ../.tmux.conf
-ln -s .vimrc ../.vimrc
-ln -s .vimrc_light ../.vimrc_light
-ln -s .vim ../.vim
+cd ../
+
+ln -s .dotfiles/.zshrc ./.zshrc
+ln -s .dotfiles/.tmux.conf ./.tmux.conf
+ln -s .dotfiles/.vimrc ./.vimrc
+ln -s .dotfiles/.vimrc_light ./.vimrc_light
+ln -s .dotfiles/.vim ./.vim
 
 echo "Finished!\\nYou have to install silversearcher-ag and pyenv.\\nDon't forget to compile vimproc after update NeoBundle plugins.\\nCaution! tmux settings are available since version 2.0"
