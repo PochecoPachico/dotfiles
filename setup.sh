1acaeb7782257cde3dde7387d2563a20738cf373
PochecoPachico <sasimi35@gmail.com>
2016-11-03 23:29:41 +0900

    remove atom settings from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ ln -sv $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sv $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -sv $HOME/.dotfiles/.vimrc_light $HOME/.vimrc_light
 ln -sv $HOME/.dotfiles/.vim $HOME/.vim
-ln -sv $HOME/.dotfiles/.atom $HOME/.atom
 
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
